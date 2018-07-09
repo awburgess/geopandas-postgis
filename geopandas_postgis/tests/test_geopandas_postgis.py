11addd726fb7efa6c4007a82a9663509582f5485
Aaron Burgess <geoburge@gmail.com>
2018-07-08 21:31:38 -0400

    Added to_postgis test setup
    Do this so that the code is in place.  Still need to add Docker handling
    to spin up a test database

--- geopandas_postgis/tests/test_geopandas_postgis.py
+++ geopandas_postgis/tests/test_geopandas_postgis.py
@@ -6,6 +6,9 @@ import typing
 from pathlib import Path
 import geopandas as gpd
 
+from sqlalchemy import create_engine
+
+import geopandas_postgis.tests.config as config
 from geopandas_postgis import geopandas_postgis as GPOST
 
 _RESOURCES = Path(__file__).parent / 'resources'
@@ -28,10 +31,33 @@ def test_extract_srid_int_from_crs(dummy_dataframe: gpd.GeoDataFrame) -> typing.
     of the epsg id.
 
     Args:
-        dummy_dataframe: Text fixture to allow all test functions to access the same object
+        dummy_dataframe: Test fixture to allow all test functions to access the same object
 
     Returns:
         None
+
     """
     test_gdf = gpd.read_file(dummy_dataframe)
     assert test_gdf.postgis._extract_srid_int_from_crs() == 4269
+
+
+def test_to_postgis(dummy_dataframe: gpd.GeoDataFrame) -> typing.NoReturn:
+    """
+    Assert that GeoDataframe is loaded to PostGIS
+
+    Args:
+        dummy_dataframe: Test fixture to allow all test functions to access the same object
+
+    Returns:
+        None
+
+    """
+    engine = create_engine(config.POSTGRES_ENGINE_STRING)
+
+    test_gdf = gpd.read_file(dummy_dataframe)
+
+    test_gdf.postgis.to_postgis(engine, 'test', 'MULTIPOLYGON')
+
+    postgis_count = engine.execute("""SELECT COUNT(*) as test_cnt FROM test""").fetchone().test_cnt
+
+    assert postgis_count == test_gdf.shape[0]
