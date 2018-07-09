70d0faaed962dd2f4f41048818395f4169460b1a
Aaron Burgess <geoburge@gmail.com>
2018-07-08 21:02:35 -0400

    Test and fixture added with some code clean-up
    Do this to continue to extend unit testing.  Still need to cover
    to_postgis

--- geopandas_postgis/geopandas_postgis.py
+++ geopandas_postgis/geopandas_postgis.py
@@ -15,18 +15,16 @@ class PostGIS:
     def __init__(self, geopandas_obj: gpd.GeoDataFrame):
         self._obj = geopandas_obj
 
-    @classmethod
-    def _extract_srid_int_from_crs(cls):
+    def _extract_srid_int_from_crs(self):
         """
         Extracts EPSG Integer from GeoDataframe crs property dictionary
 
         Returns:
             EPSG integer
         """
-        return cls._obj.crs['init'].replace('epsg:', '')
+        return int(self._obj.crs['init'].replace('epsg:', ''))
 
-    @classmethod
-    def to_postgis(cls, con: _sqlalchemy_engine, table_name: str, geometry: str, schema: str=typing.Union[str, None],
+    def to_postgis(self, con: _sqlalchemy_engine, table_name: str, geometry: str, schema: str=typing.Union[str, None],
                    transform_crs: bool=False, srid: int=4326, **kwargs) -> typing.NoReturn:
         """
         Loads a GeoDataframe to PostGIS
@@ -46,14 +44,14 @@ class PostGIS:
         Returns:
             None
         """
-        gpd_copy =  cls._obj.copy()
+        gpd_copy = self._obj.copy()
         if transform_crs:
             gpd_copy = gpd_copy.to_crs({'init': 'epsg:{}'.format(srid)})
 
-        gdf_srid = cls._extract_srid_int_from_crs()
+        gdf_srid = self._extract_srid_int_from_crs()
 
         gpd_copy['geom'] = gpd_copy.geometry.apply(lambda geom: WKTElement(geom.wkt, srid=gdf_srid))
         gpd_copy.drop('geometry', 1, inplace=True)
 
-        cls.to_sql(name=table_name, con=con, schema=schema, dtype={'geom': Geometry(geometry_type=geometry.upper(),
-                                                                                    srid=gdf_srid)}, **kwargs)
+        self._obj.to_sql(name=table_name, con=con, schema=schema, dtype={'geom': Geometry(geometry_type=geometry.upper(),
+                                                                                          srid=gdf_srid)}, **kwargs)
--- geopandas_postgis/tests/test_geopandas_postgis.py
+++ geopandas_postgis/tests/test_geopandas_postgis.py
@@ -2,7 +2,9 @@
 Tests for geopandas_postgis custom accessor
 """
 import pytest
+import typing
 from pathlib import Path
+import geopandas as gpd
 
 from geopandas_postgis import geopandas_postgis as GPOST
 
@@ -10,11 +12,26 @@ _RESOURCES = Path(__file__).parent / 'resources'
 
 
 @pytest.fixture(scope='session')
-def dummy_dataframe():
+def dummy_dataframe() -> str:
     """
-    Creates a GeoDataframe for testing from resource file
+    Creates a string reference to shp file for testing
 
     Returns:
         GeoDataframe for test purposes (uses Census U.S. State Cartographic Boundary Files 1:500,000)
     """
-    state_shp = _RESOURCES / 'cb_2017_us_state_500k.shp'
+    return (_RESOURCES / 'cb_2017_us_state_500k.shp').as_posix()
+
+
+def test_extract_srid_int_from_crs(dummy_dataframe: gpd.GeoDataFrame) -> typing.NoReturn:
+    """
+    Test that PostGIS._extract_srid_int_from_crs() returns the integer representation
+    of the epsg id.
+
+    Args:
+        dummy_dataframe: Text fixture to allow all test functions to access the same object
+
+    Returns:
+        None
+    """
+    test_gdf = gpd.read_file(dummy_dataframe)
+    assert test_gdf.postgis._extract_srid_int_from_crs() == 4269
